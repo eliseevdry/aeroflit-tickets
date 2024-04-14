fab8addcb485c3ba40a0321a89307e43dde1f7bb
Andrey Eliseev <andrey.s.eliseev@lenta.com>
2024-04-14 13:00:48 +0300

    007: ResultSet. SELECT

--- src/org/eliseev/aeroflot/tickets/TicketsRunner.java
+++ src/org/eliseev/aeroflot/tickets/TicketsRunner.java
@@ -3,20 +3,31 @@ package org.eliseev.aeroflot.tickets;
 import org.eliseev.aeroflot.tickets.utils.PgConnectionManager;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TicketsRunner {
     public static void main(String[] args) {
         String sql = """
-                UPDATE flight
-                SET cancelled = true
-                WHERE id in (SELECT id FROM flight ORDER BY random() LIMIT 1000)
+                SELECT *
+                FROM flight
+                ORDER BY random()
+                LIMIT 100
                 """;
         try (Connection con = PgConnectionManager.open();
              Statement stmt = con.createStatement()) {
-            int executeResult = stmt.executeUpdate(sql);
-            System.out.println(executeResult);
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                String result = "--- id = " +
+                                rs.getLong("id") + " ---\n" +
+                                "aircraft_id = " + rs.getInt("aircraft_id") + "\n" +
+                                "path_id = " + rs.getInt("path_id") + "\n" +
+                                "departure_date = " + rs.getTimestamp("departure_date") + "\n" +
+                                "arrival_date = " + rs.getTimestamp("arrival_date") + "\n" +
+                                "cancelled = " + rs.getBoolean("cancelled") + "\n";
+                System.out.println(result);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
