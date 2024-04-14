0822a879152971a9f99220ed33791d4bddcd3c55
Andrey Eliseev <andrey.s.eliseev@lenta.com>
2024-04-14 13:41:15 +0300

    009: SQL injection

--- src/org/eliseev/aeroflot/tickets/TicketsRunner.java
+++ src/org/eliseev/aeroflot/tickets/TicketsRunner.java
@@ -6,26 +6,31 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketsRunner {
     public static void main(String[] args) {
+        String flightId = "45000 OR 1 = 1";
+        System.out.println(getTicketsId(flightId));
+    }
+
+    private static List<Long> getTicketsId(String flight_id) {
         String sql = """
-                INSERT INTO city(country_name, name)
-                VALUES
-                ('Russia', 'Moscow'),
-                ('Russia', 'Voronezh');
-                """;
+                SELECT id
+                FROM ticket
+                WHERE flight_id = %s;
+                """.formatted(flight_id);
+        List<Long> result = new ArrayList<>();
         try (Connection con = PgConnectionManager.open();
              Statement stmt = con.createStatement()) {
-            int count = stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs = stmt.getGeneratedKeys();
-            System.out.println("Update count: " + count);
-            while (rs.next()) {
-                String result = "--- id = " + rs.getInt("id") + " ---\n";
-                System.out.print(result);
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                result.add(rs.getObject("id", Long.class));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return result;
     }
 }
